2723e6fa422c984359aaa28691337df6f80565a8
frankleysantos <frankleysilvasantos@gmail.com>
2018-08-21 09:47:38 -0300

    calculo de data

--- calcular2.php
+++ calcular2.php
@@ -2,7 +2,7 @@
 include "config.php";
 $hora_inicio = "07:45:00";  
 $hora_fim = "12:00:00";              
-$pessoa = 330;
+$pessoa = 1000;
 $porminuto = 10;
 $data = "08/08/2018";
 $ini = strtotime($hora_inicio);
@@ -13,14 +13,24 @@ $count = 0;
 $i = 0;
 for ($pessa = 0; $pessa < $pessoa; $pessa++) {
 	    $atu = strtotime('+15 minutes', $atu);
+        $final = strtotime("18:15:00");
+                if ($atu == $final) {
+                    $atu = strtotime("08:00:00");
+                    if ($atu == strtotime("08:00:00")) {
+                        $data = date('d/m/Y', strtotime('+1 days', strtotime($data)));
+                    }                    
+                }
+                if ($atu == strtotime("08:00:00")) {
+                        $data = date('d/m/Y', strtotime('+1 days', strtotime($data)));
+                    }
      	for ($pessoaini=0; $pessoaini < $porminuto; $pessoaini++) {
      	    $count ++; 
      	    if ($fim == $atu) {
      	    	$atu = strtotime("14:00:00");
-                $final = strtotime("18:00:00");
+        
      	    	if ($count <= $pessoa) {
      	    	$hora = date('H:i', $atu);
-                echo $count.$hora."<br>";
+                echo $count.$hora.$data."<br>";
                 /*
                 $sql = $pdo->prepare("INSERT INTO atendimento (horario, id_pessoa, data) VALUES (:horario, :id_pessoa, :data)");
                 $sql ->bindValue(":horario", $hora);
@@ -30,12 +40,12 @@ for ($pessa = 0; $pessa < $pessoa; $pessa++) {
                 $sql ->bindValue(":horario", $hora);
                 $sql ->bindValue(":id_cargo", $count);
                 $sql ->bindValue(":data", $data);
-                $sql ->execute();
+                $sql ->execute();   
      	    }
      	    }else{
      	    if ($count <= $pessoa) {
      	    	$hora = date('H:i', $atu);
-                echo $count.$hora."<br>";
+                echo $count.$hora.$data."<br>";
                 /*
                 $sql = $pdo->prepare("INSERT INTO atendimento (horario, id_pessoa, data) VALUES (:horario, :id_pessoa, :data)");
                 $sql ->bindValue(":horario", $hora);

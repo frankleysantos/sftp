b966d32d52401c8420ef8f8175ac2a887a5605b8
frankleysantos <frankleysilvasantos@gmail.com>
2018-08-21 08:44:31 -0300

    alterações

--- login.php
+++ login.php
@@ -32,27 +32,12 @@ $msn = $_GET['msn'];
 echo "<label class='btn btn-success form-control'>".$msn."</label>";
 }
 ?>
-<form method="POST">
-	<legend>Processo Seletivo Simplificado 2018 - Secretaria Municipal de Assistência Social e Habitação</legend>
-
-	<div class="form-group">
-		<label class="fa fa-user">&ensp;CPF</label>
-		<input type="text" class="form-control" name="cpf" placeholder="Digite o seu CPF" maxlength="11" minlength="11" onblur="return verificarCPF(this.value)" onkeypress="return event.charCode >= 48 && event.charCode <= 57">
-	</div>
-	<div class="form-group">
-		<label class="fa fa-unlock">&ensp;Senha</label>
-		<input type="password" class="form-control" name="senha" placeholder="Digite sua Senha">
-	</div>
-
-	<button type="submit" class="btn btn-primary fa fa-sign-in">&ensp;Entrar</button>
-</form>
-
-
+       <hr>
         <div class="panel-group row" id="accordion" role="tablist" aria-multiselectable="true">
          <div class="panel-default col-md-6 btn-primary">
           <div class="panel-heading" role="tab" id="headingOne">
            <h4 class="panel-title">
-            <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">Cadastre seu Login!</a>
+            <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne"><b>Cadastre seu Login. Clique aqui!</b></a>
            </h4>
           </div>
           <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
@@ -74,7 +59,7 @@ echo "<label class='btn btn-success form-control'>".$msn."</label>";
          <div class="panel-default col-md-6 btn-danger">
           <div class="panel-heading" role="tab" id="headingTwo">
            <h4 class="panel-title">
-            <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">Esqueceu a senha?</a>
+            <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo"><b>Esqueceu a senha? Clique Aqui.</b></a>
            </h4>
           </div>
           <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
@@ -97,6 +82,25 @@ echo "<label class='btn btn-success form-control'>".$msn."</label>";
           </div>
          </div>
         </div>
+        <hr>
+
+<form method="POST">
+	<legend>Processo Seletivo Simplificado 2018 - Secretaria Municipal de Assistência Social e Habitação</legend>
+
+	<div class="form-group">
+		<label class="fa fa-user">&ensp;CPF</label>
+		<input type="text" class="form-control" name="cpf" placeholder="Digite o seu CPF" maxlength="11" minlength="11" onblur="return verificarCPF(this.value)" onkeypress="return event.charCode >= 48 && event.charCode <= 57">
+	</div>
+	<div class="form-group">
+		<label class="fa fa-unlock">&ensp;Senha</label>
+		<input type="password" class="form-control" name="senha" placeholder="Digite sua Senha">
+	</div>
+
+	<button type="submit" class="btn btn-primary fa fa-sign-in">&ensp;Entrar</button>
+</form>
+
+
+
         <legend>Editais</legend>
         <p><a href="editais/Edital1.pdf" target="_blank" class="form-control btn-default"><b>1 - Edital I</b></a></p>
         <p><a href="editais/Edital2.pdf" target="_blank" class="form-control btn-default"><b>2 - Edital II</b></a></p>
